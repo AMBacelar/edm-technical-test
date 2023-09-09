c1eaabafd715d0c84d55a893ea982c180197d5ba
AMBacelar <a.m.bacelar@gmail.com>
2023-09-09 15:59:16 +0100

    basic database created

--- server/app.js
+++ server/app.js
@@ -1,9 +1,42 @@
-const express = require("express");
+import express from "express";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
+import { faker } from "@faker-js/faker";
+
+export function createRandomUser() {
+  return {
+    username: faker.internet.userName(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+  };
+}
+export const users = faker.helpers.multiple(createRandomUser, {
+  count: 10,
+});
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const file = join(__dirname, "db.json");
+const adapter = new JSONFile(file);
+const defaultData = users;
+
+const db = new Low(adapter, defaultData);
+
+await db.read();
+await db.write();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+  res.send(db.data);
 });
 
 app.listen(port, () => {
